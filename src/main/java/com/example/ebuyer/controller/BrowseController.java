6dc5528a170c9161afaa4b930b4ef6b52e0af050
Roman Belnitskiy <roman.belnitskiy@gmail.com>
2024-01-29 21:26:16 +0200

    added request

---
+++ src/main/java/com/example/ebuyer/controller/BrowseController.java
@@ -0,0 +1,22 @@
+package com.example.ebuyer.controller;
+
+import com.example.ebuyer.client.ApiException;
+import com.example.ebuyer.client.model.SearchPagedCollection;
+import com.example.ebuyer.service.BrowseService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+public class BrowseController {
+    @Autowired
+    private BrowseService service;
+
+    @GetMapping
+    public ResponseEntity<SearchPagedCollection> find() throws ApiException {
+        return ResponseEntity.ok(service.find());
+    }
+}
