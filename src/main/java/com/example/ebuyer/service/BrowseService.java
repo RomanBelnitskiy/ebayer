fa820a698b62b15377ef014e4b74b20674cbb395
Roman Belnitskiy <roman.belnitskiy@gmail.com>
2024-02-01 20:25:17 +0200

    added method

--- src/main/java/com/example/ebuyer/service/BrowseService.java
+++ src/main/java/com/example/ebuyer/service/BrowseService.java
@@ -2,6 +2,7 @@ package com.example.ebuyer.service;
 
 import com.example.ebuyer.client.ApiException;
 import com.example.ebuyer.client.api.ItemSummaryApi;
+import com.example.ebuyer.client.model.RequestParams;
 import com.example.ebuyer.client.model.SearchPagedCollection;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -12,26 +13,13 @@ public class BrowseService {
     private ItemSummaryApi api;
 
     public SearchPagedCollection find() throws ApiException {
-        String aspectFilter = null;
-        String autoCorrect = null;
-        String categoryIds = null;
-        String charityIds = null;
-        String compatibilityFilter = null;
-        String epid = null;
-        String fieldgroups = null;
-        String filter = null;
-        String gtin = null;
-        String limit = "3";
-        String offset = null;
-        String q = "RAM";
-        String sort = null;
-        String X_EBAY_C_ENDUSERCTX = null;
-        String X_EBAY_C_MARKETPLACE_ID = "EBAY_US";
-        SearchPagedCollection response = api.search(aspectFilter,
-                autoCorrect, categoryIds, charityIds, compatibilityFilter,
-                epid, fieldgroups, filter, gtin, limit, offset, q, sort,
-                X_EBAY_C_ENDUSERCTX, X_EBAY_C_MARKETPLACE_ID);
+        RequestParams params = RequestParams
+                .builder()
+                .q("ram")
+                .limit("10")
+                .X_EBAY_C_MARKETPLACE_ID("EBAY_US")
+                .build();
 
-        return response;
+        return api.search(params);
     }
 }
