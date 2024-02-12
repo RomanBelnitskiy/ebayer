b89bff0451d1c8538a6adc6789334c89e6bb6eb7
Тимченко Влад <timchenko.vld@gmail.com>
2024-02-12 13:59:23 +0200

    added request params dto

--- src/main/java/com/example/ebuyer/service/BrowseService.java
+++ src/main/java/com/example/ebuyer/service/BrowseService.java
@@ -2,7 +2,7 @@ package com.example.ebuyer.service;
 
 import com.example.ebuyer.client.ApiException;
 import com.example.ebuyer.client.api.ItemSummaryApi;
-import com.example.ebuyer.client.model.RequestParams;
+import com.example.ebuyer.client.dto.RequestParamsDto;
 import com.example.ebuyer.client.model.SearchPagedCollection;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -12,12 +12,12 @@ public class BrowseService {
     @Autowired
     private ItemSummaryApi api;
 
-    public SearchPagedCollection find(RequestParams params) throws ApiException {
+    public SearchPagedCollection find(RequestParamsDto params) throws ApiException {
         return api.search(params);
     }
 
     public SearchPagedCollection getAspectAndCategoryRefinements() throws ApiException {
-        RequestParams params = RequestParams
+        RequestParamsDto params = RequestParamsDto
                 .builder()
                 .q("memory")
                 .limit("10")
